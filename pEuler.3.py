6c0d5a282ba7f8cbc9e5d8c3d4856a915ecbb2f7
neven-h <69196529+neven-h@users.noreply.github.com>
2021-04-28 13:31:05 +0300

    Update and rename pEuler.2.py to pEuler.3.py

--- pEuler.3.py
+++ pEuler.3.py
@@ -1,9 +1,13 @@
-def is_prime(p):
-    if p % 2 == 0:
+import math
+
+
+def is_prime(n):
+    if n % 2 == 0:
         return False
     else:
-        for i in range(2, (p // 2 + 1)):
-            if p % i == 0:
+        t = math.sqrt(n)
+        for i in range(2, int(t)):
+            if n % i == 0:
                 return False
         return True
 
@@ -13,9 +17,12 @@ def prime_divisors(n):
     if is_prime(n):
         return n
     else:
-        for k in range(3, (n // 2 + 1), 2):
+        t = int(math.sqrt(n))
+        for k in range(3, t , 2):
             if is_prime(k):
                 if n / k == int(n / k) and k > max_prime:
                     max_prime = k
         return max_prime
+
+
 print(prime_divisors(600851475143))
