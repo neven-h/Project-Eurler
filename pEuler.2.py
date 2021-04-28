bd2eb9fa1baeeb762f13f4909a86f93a09f5d8c8
neven-h <69196529+neven-h@users.noreply.github.com>
2021-04-28 11:34:30 +0300

    Update pEuler.2.py

--- pEuler.2.py
+++ pEuler.2.py
@@ -1,33 +1,21 @@
-# Project Euler problem 2
-import math
+def is_prime(p):
+    if p % 2 == 0:
+        return False
+    else:
+        for i in range(2, (p // 2 + 1)):
+            if p % i == 0:
+                return False
+        return True
 
 
-def fibonacci_recu(n):
-    if n <= 1:
+def prime_divisors(n):
+    max_prime = 2
+    if is_prime(n):
         return n
     else:
-        return fibonacci_recu(n - 1) + fibonacci_recu(n - 2)
-
-
-# find the index of the fibonacchi number which is our upper bound
-def find_fibonacchi_bound():
-    k = 5
-    bound = 4000000
-    while fibonacci_recu(k) < bound:
-        k += 1
-    else:
-        return k
-
-
-def fibonacci_even_terms():
-    upper_range = find_fibonacchi_bound()
-    summtion = 0
-    fibo_list = []
-    for i in range(2, upper_range):
-        if fibonacci_recu(i) % 2 == 0:
-            fibo_list.append(fibonacci_recu(i))
-    summtion = sum(fibo_list)
-    print(summtion)
-
-
-fibonacci_even_terms()
+        for k in range(3, (n // 2 + 1), 2):
+            if is_prime(k):
+                if n / k == int(n / k) and k > max_prime:
+                    max_prime = k
+        return max_prime
+print(prime_divisors(600851475143))
