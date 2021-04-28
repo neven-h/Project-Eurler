09ac122e654b7ac55009b11b12962daf22975480
neven-h <69196529+neven-h@users.noreply.github.com>
2021-04-28 19:29:14 +0300

    Add files via upload

---
+++ pEuler4.py
@@ -0,0 +1,12 @@
+def compare_digits():
+    max_palindrom = 100000
+    for x in range(1000, 101, -1):
+        for y in range(1000, 101, -1):
+            z: int = x * y
+            z_str = str(z)
+            if z_str == z_str[::-1] and z > max_palindrom:
+                max_palindrom = z
+    return max_palindrom
+
+
+print(compare_digits())
