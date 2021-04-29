ca50dd90f65205c1b65dc088feebaec05195747b
neven-h <69196529+neven-h@users.noreply.github.com>
2021-04-29 11:37:28 +0300

    Update pEuler.2.py

--- pEuler.2.py
+++ pEuler.2.py
@@ -1,33 +1,48 @@
 # Project Euler problem 2
 import math
 
-
-def fibonacci_recu(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci_recu(n - 1) + fibonacci_recu(n - 2)
+fib_usage_counter = 0
 
 
-# find the index of the fibonacchi number which is our upper bound
-def find_fibonacchi_bound():
+# def fibonacci_recu(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return fibonacci_recu(n - 1) + fibonacci_recu(n - 2)
+
+# calculating the Fibonacci sequence without recursion
+def fib(n: int) -> int:
+    global fib_usage_counter
+    fib_usage_counter += 1
+    if n == 0: return 0
+    value1, value2 = 1, 1
+    while n > 2:
+        value1, value2 = value2, value1 + value2
+        n -= 1
+    return value2
+
+
+# find the index of the Fibonacchi number which is our upper bound
+def find_fibonacci_bound():
     k = 5
     bound = 4000000
-    while fibonacci_recu(k) < bound:
+    while fib(k) < bound:
         k += 1
     else:
         return k
 
 
 def fibonacci_even_terms():
-    upper_range = find_fibonacchi_bound()
+    upper_range = find_fibonacci_bound()
     summtion = 0
     fibo_list = []
     for i in range(2, upper_range):
-        if fibonacci_recu(i) % 2 == 0:
-            fibo_list.append(fibonacci_recu(i))
+        if fib(i) % 2 == 0:
+            fibo_list.append(fib(i))
     summtion = sum(fibo_list)
     print(summtion)
 
 
-fibonacci_even_terms()
+print(fibonacci_even_terms())
+print(f'fib_usage_counter: {fib_usage_counter}')
+# fib_usage_counter: 73
