6bcc9d9d239df62d10975544b985150f2b77fe6d
neven-h <69196529+neven-h@users.noreply.github.com>
2021-04-29 11:59:38 +0300

    Update pEuler.3.py

--- pEuler.3.py
+++ pEuler.3.py
@@ -1,5 +1,7 @@
 import math
 from is_prime import is_prime
+from pEuler7 import the_nth_prime
+
 
 def prime_divisors(n):
     max_prime = 2
@@ -7,7 +9,10 @@ def prime_divisors(n):
         return n
     else:
         t = int(math.sqrt(n))
-        for k in range(3, t , 2):
+        for k in range (the_nth_prime(1000), the_nth_prime(10000), 2): #the 1000th prime number is not a divisor of n,
+            # the_nth_prime(10000) is some upper bound I've chosen and improves performances significantly but I wonder
+            # if there is some closed formula for a lower and upper bound for this problem
+        #for k in range(1051, t // 2, 2):  # I've chosen to start from 1051 which is prime and is not a divisor of 600851475143
             if is_prime(k):
                 if n / k == int(n / k) and k > max_prime:
                     max_prime = k
